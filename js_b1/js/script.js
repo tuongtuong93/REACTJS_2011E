8e8a13598782a0f9fe1990cb2dba918553546994
tuongdzz <74662880+tuongdzz@users.noreply.github.com>
2020-11-26 19:13:02 +0700

    update

---
+++ js_b1/js/script.js
@@ -0,0 +1,16 @@
+
+hello = () => {
+    let a=100,b=200;
+    console.log(a+b);
+}
+hello();
+
+const arrNumb = [1,2,3,4,5,6,7,8,9];
+console.log("Lenght :" + arrNumb + ''+" pt7 :"+'' + arrNumb[7]);
+
+arrNumb.splice(1, 5, 10, 20, 30, 50, 100);
+console.log(arrNumb);
+
+const arrNumb2 = [5, 7, 15, 25, 50];
+const map1 = arrNumb2.map(x => x * 3);
+console.log(map1);
